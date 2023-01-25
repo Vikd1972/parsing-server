c0951f26d536c1a69980f69997202e72e855e3a2
Viktor Dmitriev <vikdmitriev72@gmail.com>
2023-01-25 11:20:47 +0300

    added parsing

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 import config from './config';
-import job from './utils/cron-ping';
+
+// import job from './utils/cron-ping';
+import decodeString from './utils/decodeString';
+import parsingString from './utils/parsingString';
 
 const app = express();
 
-job.start();
+fetch('http://www.tgnvoda.ru/avarii.php')
+  .then((res: any) => res.buffer())
+  .then((res: string) => decodeString(res))
+  .then((res: string) => parsingString(res))
+  .catch((err: any) => console.error('\n--------------------\nБля!\n\n', err));
+
+// job.start();
 
 void (async () => {
   try {
