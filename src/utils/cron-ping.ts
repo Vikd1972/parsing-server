a73dee01a625681c3b401a7184ca482457eb90f8
Viktor Dmitriev <vikdmitriev72@gmail.com>
2023-01-26 11:53:23 +0300

    fix bug

--- src/utils/cron-ping.ts
+++ src/utils/cron-ping.ts
@@ -1,19 +1,29 @@
-import fetch from 'node-fetch';
+import nodeFetch from 'node-fetch';
 import { CronJob } from 'cron';
 
-import decodeString from './decodeString';
-import parsingString from './parsingString';
+// import decodeString from './decodeString';
+// import parsingString from './parsingString';
 
 const job = new CronJob(
   '*/5 * * * * *',
   (() => {
-    fetch('http://www.tgnvoda.ru/avarii.php')
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .then((res: any) => res.buffer())
-      .then((res: string) => decodeString(res))
-      .then((res: string) => parsingString(res))
-      .catch((err: string) => console.error(err));
+    (async () => {
+      try {
+        const response = await nodeFetch('http://www.tgnvoda.ru/avarii.php');
+        const html = await response.textConverted();
+        // eslint-disable-next-line no-console
+        console.log(html);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
+    })();
+    // fetch('http://www.tgnvoda.ru/avarii.php')
+    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //   .then((res: Response) => res.buffer())
+    //   .then((res: string) => decodeString(res))
+    //   .then((res: string) => parsingString(res))
+    //   .catch((err: string) => console.error(err));
   }),
 );
-
 export default job;
