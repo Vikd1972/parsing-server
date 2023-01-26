e9d58a53a6a6847f2af6ea1338b67efd8199d113
Viktor Dmitriev <vikdmitriev72@gmail.com>
2023-01-26 13:51:00 +0300

    changed decoding

--- src/utils/cron-ping.ts
+++ src/utils/cron-ping.ts
@@ -1,29 +1,21 @@
 import nodeFetch from 'node-fetch';
 import { CronJob } from 'cron';
 
-// import decodeString from './decodeString';
-// import parsingString from './parsingString';
+import parsingString from './parsingString';
 
 const job = new CronJob(
-  '*/5 * * * * *',
+  '0 */10 * * * *',
   (() => {
     (async () => {
       try {
         const response = await nodeFetch('http://www.tgnvoda.ru/avarii.php');
         const html = await response.textConverted();
-        // eslint-disable-next-line no-console
-        console.log(html);
+        parsingString(html);
       } catch (error) {
         // eslint-disable-next-line no-console
         console.log(error);
       }
     })();
-    // fetch('http://www.tgnvoda.ru/avarii.php')
-    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    //   .then((res: Response) => res.buffer())
-    //   .then((res: string) => decodeString(res))
-    //   .then((res: string) => parsingString(res))
-    //   .catch((err: string) => console.error(err));
   }),
 );
 export default job;
