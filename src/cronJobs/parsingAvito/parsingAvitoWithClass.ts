82f9011dc7fd295af795e448663b1a021328578c
Viktor Dmitriev <vikdmitriev72@gmail.com>
2023-02-01 15:11:36 +0300

    added user emulation & parsing with pagination

--- src/cronJobs/parsingAvito/parsingAvitoWithClass.ts
+++ src/cronJobs/parsingAvito/parsingAvitoWithClass.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable max-len */
 import puppeteer from 'puppeteer-core';
 import { executablePath } from 'puppeteer';
 import type { Page, Browser } from 'puppeteer-core';
@@ -17,6 +19,8 @@ const notesList: Array<{
 
 let page: Page;
 let browser: Browser;
+let numberOfPages: number;
+let currentPage = 1;
 
 class Jobs {
   static async init() {
@@ -28,44 +32,66 @@ class Jobs {
     });
     page = await browser.newPage();
     page.setDefaultNavigationTimeout(0);
+    await page.setViewport({
+      width: 1200,
+      height: 800,
+    });
     await page.goto(jobUrl);
+    const numbers = await page.$('[class^="page-title-count-"]');
+    const numberOfAds = +await numbers.evaluate((el) => el.textContent);
+    numberOfPages = Math.ceil(numberOfAds / 50);
+  }
+
+  static async userEmulator(page: Page) {
+    const distance = 100; // should be less than or equal to window.innerHeight
+    const delay = 100;
+    while (await page.evaluate(() => document.scrollingElement.scrollTop + window.innerHeight < document.scrollingElement.scrollHeight)) {
+      await page.evaluate((y) => { document.scrollingElement.scrollBy(0, y); }, distance);
+      await page.waitForTimeout(delay);
+    }
+    await page.waitForTimeout(3000);
   }
 
   static async resolve() {
     await this.init();
-    const notes = await page.$$('[class^="iva-item-body-"]');
-    const notesArr = Array.from(notes);
 
-    // notesArr.map(async (note) => {
-    for (let i = 0; i < notesArr.length; i++) {
-      // const result = await note.evaluate((el) => {
-      // eslint-disable-next-line no-await-in-loop
-      const result = await notesArr[i].evaluate((el) => {
-        const title = el.querySelector('[class^="title-root-"]').textContent;
-        const price = el.querySelector('[class^="price-text-"]').textContent;
-        const address = el.querySelector('[class^="geo-root-"]').textContent;
-        const description = el.querySelector('[class^="iva-item-text-"]').textContent;
-        const date = el.querySelector('[class^="date-root-"]').textContent;
-        return {
-          title,
-          price,
-          address,
-          description: description.split('\n').join().replace(',,', ''),
-          date,
-        };
-      });
-      notesList.push(result);
+    while (currentPage < numberOfPages) {
+      const notes = await page.$$('[class^="iva-item-body-"]');
+      const notesArr = Array.from(notes);
+
+      for (let i = 0; i < notesArr.length; i++) {
+        const result = await notesArr[i].evaluate((el) => {
+          const title = el.querySelector('[class^="title-root-"]').textContent;
+          const price = el.querySelector('[class^="price-text-"]').textContent;
+          const address = el.querySelector('[class^="geo-root-"]').textContent;
+          const description = el.querySelector('[class^="iva-item-text-"]').textContent;
+          const date = el.querySelector('[class^="date-root-"]').textContent;
+          return {
+            title,
+            price,
+            address,
+            description: description.split('\n').join().replace(',,', ''),
+            date,
+          };
+        });
+        notesList.push(result);
+      }
+      await this.userEmulator(page);
+      await page.click('[data-marker^="pagination-button/next"]');
+      await page.reload({ waitUntil: ['networkidle0', 'domcontentloaded'] });
+      currentPage++;
     }
-    // });
+    // eslint-disable-next-line no-console
+    console.log('notesList.length', notesList.length);
     // eslint-disable-next-line no-console
     console.log('notesList', notesList);
+    await this.userEmulator(page);
   }
 
   static async getJobs() {
     const jobs = await this.resolve();
     await browser.close();
     log.step(0, 0, 0, 1);
-    // console.log(jobs)
     return jobs;
   }
 }
